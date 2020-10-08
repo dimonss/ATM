011e60c6841cc1cc21c4eb0acea9584eaf4aa569
DiCh <faimos69@gmail.com>
2020-10-08 22:50:56 +0600

    add sound end img card

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+function soundPlay(track){
+	var audio = new Audio();
+	audio.preload = 'auto';
+	audio.src = track;
+	audio.play();
+}
+
 function insert(amount){
+	soundPlay('p.mp3');
 	if(amount == 0)
 		if(document.getElementById('mainWindow').value =="")
 			return;
@@ -6,6 +14,7 @@ function insert(amount){
 		document.getElementById('mainWindow').value = document.getElementById('mainWindow').value + amount;
 }
 function back(){
+	soundPlay('p.mp3');
 	var buf = document.getElementById("mainWindow").value;
 	document.getElementById("mainWindow").value = buf.substring(0, buf.length-1);
 }
@@ -19,18 +28,29 @@ function testLength(){
 }
 
 function result(){
+	soundPlay('ejectCash.mp3');
 	document.getElementById("textBox").value = "";
 	typeOfBanknote=[5000, 2000, 1000, 500, 200, 100, 50, 20];
-	for (var i = 0; i < 7; i++) {
+	for (var i = 0; i < 8; i++) {
 		while(1){
 			if(document.getElementById("mainWindow").value - typeOfBanknote[i] >= 0){
-				document.getElementById("textBox").value = document.getElementById("textBox").value + typeOfBanknote[i]; 
+				document.getElementById("textBox").value = document.getElementById("textBox").value +" " + typeOfBanknote[i]; 
 				document.getElementById("mainWindow").value = document.getElementById("mainWindow").value - typeOfBanknote[i];
-				alert(true);
 			}
 			else{
-				alert(false);
 				break;}
 		}
 	}
+}
+
+var statusCard = true;
+function includeCard(){
+	if(statusCard == true){
+		soundPlay('inputPin.mp3');
+		document.getElementById("imgCard").style="opacity:0"; 
+	}
+	else{
+		document.getElementById("imgCard").style="opacity:1";
+	}
+	statusCard = !statusCard;
 }
