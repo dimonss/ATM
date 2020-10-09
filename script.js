2e4e3673f49a0a690a84a9102ffbea1402c9ff3d
DiCh <faimos69@gmail.com>
2020-10-09 14:00:06 +0600

    add audio and reqquest password

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function startFunc(){
+	document.getElementById("mainWindow").value = "insert the card";
+}
+
 function soundPlay(track){
 	var audio = new Audio();
 	audio.preload = 'auto';
@@ -6,17 +10,21 @@ function soundPlay(track){
 }
 
 function insert(amount){
-	soundPlay('p.mp3');
-	if(amount == 0)
-		if(document.getElementById('mainWindow').value =="")
-			return;
-	if(testLength())
-		document.getElementById('mainWindow').value = document.getElementById('mainWindow').value + amount;
+	if(statusCard == true){
+		soundPlay('p.mp3');
+		if(amount == 0)
+			if(document.getElementById('mainWindow').value =="")
+				return;
+		if(testLength())
+			document.getElementById('mainWindow').value = document.getElementById('mainWindow').value + amount;
+	}
 }
 function back(){
-	soundPlay('p.mp3');
-	var buf = document.getElementById("mainWindow").value;
-	document.getElementById("mainWindow").value = buf.substring(0, buf.length-1);
+	if(statusCard == true){
+		soundPlay('p.mp3');
+		var buf = document.getElementById("mainWindow").value;
+		document.getElementById("mainWindow").value = buf.substring(0, buf.length-1);
+	}
 }
 
 function testLength(){
@@ -26,31 +34,52 @@ function testLength(){
 	else
 		return 0;
 }
-
+var pinValueTrue = false;
 function result(){
-	soundPlay('ejectCash.mp3');
-	document.getElementById("textBox").value = "";
-	typeOfBanknote=[5000, 2000, 1000, 500, 200, 100, 50, 20];
-	for (var i = 0; i < 8; i++) {
-		while(1){
-			if(document.getElementById("mainWindow").value - typeOfBanknote[i] >= 0){
-				document.getElementById("textBox").value = document.getElementById("textBox").value +" " + typeOfBanknote[i]; 
-				document.getElementById("mainWindow").value = document.getElementById("mainWindow").value - typeOfBanknote[i];
+	if(statusCard == true){
+		if(pinValueTrue == false){
+			if(document.getElementById("mainWindow").value == "1234"){
+				pinValueTrue = true;
+				document.getElementById("mainWindow").value = "successful"
+				setTimeout(function() { document.getElementById("mainWindow").value = "";}, 1000);
 			}
 			else{
-				break;}
+				document.getElementById("mainWindow").value = "password fail"
+				setTimeout(function() { document.getElementById("mainWindow").value = "";}, 1000);
+			}
+		}
+		else{
+			soundPlay('ejectCash.mp3');
+			document.getElementById("textBox").value = "";
+			typeOfBanknote=[5000, 2000, 1000, 500, 200, 100, 50, 20];
+			for (var i = 0; i < 8; i++) {
+				while(1){
+					if(document.getElementById("mainWindow").value - typeOfBanknote[i] >= 0){
+						document.getElementById("textBox").value = document.getElementById("textBox").value +" " + typeOfBanknote[i]; 
+						document.getElementById("mainWindow").value = document.getElementById("mainWindow").value - typeOfBanknote[i];
+					}
+					else{
+						break;}
+				}
+			}
 		}
 	}
 }
 
-var statusCard = true;
+var statusCard = false;
 function includeCard(){
-	if(statusCard == true){
-		soundPlay('inputPin.mp3');
+	if(statusCard == false){
+		setTimeout(function() {soundPlay('inputPin.mp3');}, 2000);
+		soundPlay("giveTheCard.mp3")
 		document.getElementById("imgCard").style="opacity:0"; 
+		document.getElementById("mainWindow").value = "";
 	}
 	else{
 		document.getElementById("imgCard").style="opacity:1";
+		pinValueTrue = false;
+		startFunc();
+		document.getElementById("textBox").value = "";
+		soundPlay("giveTheCard.mp3");
 	}
 	statusCard = !statusCard;
 }
